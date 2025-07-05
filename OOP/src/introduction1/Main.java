fe34493964a9fa289db1537791b216c4bc73e338
Sarankumar-dot <rush4925@gmail.com>
2025-07-05 18:33:48 +0530

    init commit

---
+++ OOP/src/introduction1/Main.java
@@ -0,0 +1,146 @@
+package introduction1;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] roll = new int[5];
+        String[] name = new String[5];
+        String[] gender = new String[5];
+        
+
+
+        // if we want to add those above three in a single line you need classes
+
+        // to call the below class we use
+        Student[] details = new Student[5];
+
+        // obj creation
+        Student saran = new Student(13,"saraaaan",99.99f); // here Student() is a constructor
+        // default values without assigning before
+
+//        saran.rno = 11;
+//        saran.name = "saran";
+//        saran.marks = 88.66f;
+
+        System.out.println(saran.rno);
+        System.out.println(saran.name);
+        System.out.println(saran.marks);
+
+        // changing the name
+        saran.changename("kumar");
+        saran.greeting();
+
+
+
+        // int the object saran the Student() is a constructor, it is a function that runs when you create a class and allocate some variables
+        // you can use the constructor to add values into the objects
+        // for now we create a new stud called malik
+
+        System.out.println();
+
+        Student malik = new Student();
+        System.out.println(malik.rno);
+        System.out.println(malik.name);
+        System.out.println(malik.marks);
+
+        malik.greeting();
+
+        System.out.println();
+        // random object
+        Student random = new Student(malik);
+        System.out.println(random.rno);
+        System.out.println(random.name);
+
+
+        // calling a constructor from another constructor
+        // for this i have created a new class so that it will not get confused
+
+        Employee Ali = new Employee(1,"saran");
+        Employee Ali2 = new Employee();
+
+        System.out.println(Ali.empid);
+        System.out.println(Ali2.empid);
+        System.out.println(Ali.name);
+        System.out.println(Ali2.name);
+
+
+        System.out.println();
+        // memory allocation of new keyword
+        // here we  are creating two new objects in which one object is equal to another object
+        // if we make changes in one object it will the reflected in the another object also
+
+        Student one = new Student(1,"Saran",89.33f);
+        Student two = one;
+
+        System.out.println(one.marks);
+
+    }
+}
+// to create a class you need
+// class is basically a template of an object && an object is an instance of a class
+// real life examples of classes are car,fruits
+class Student{
+    // all the variables that  are inside your object is called instance varibles[instance means reference or example]
+    int rno;
+    String name;
+    float marks;
+
+    // printing hello bro using objects and this keyword
+    void greeting(){
+        System.out.println("hello broo " + this.name);
+    }
+
+    // changing the name of an object using this keyword
+    void changename(String naae){
+        this.name = naae;
+    }
+
+    // when you create an object with 0 values as parameter the system uses this constructor
+    Student(){
+        // for assigning the rno,name,marks we use this keyword
+        // this keyword is replacing it with the name of the reference variable eg( saran , malik )
+        this.rno=1;
+        this.name="noobie";
+        this.marks = 99.99f;
+    }
+
+    // when you create an object with 3 values as parameter the system uses this constructor
+    Student(int roll , String name , float marks){
+        // for assigning the rno,name,marks we use this keyword
+        // this keyword is replacing it with the name of the reference variable eg( saran , malik )
+
+        // """this is the best method we can add all values in one line"""
+        this.rno=roll;
+        this.name=name;
+        this.marks =marks;
+    }
+
+    // we can also get the other object values as an input for our current object lets see how
+    // for this we are creating an object called random
+    Student (Student other){
+        this.rno = other.rno;
+        this.name = other.name;
+        this.marks = other.marks;
+    }
+}
+
+
+class Employee{
+    int empid = 0;
+    String name = "default";
+
+    Employee(int empid , String name){
+        this.empid = empid;
+        this.name=name;
+    }
+
+    Employee(){
+        // this is how you can call one constructor from another construtor
+        // internally it is Ali(1,"saran"); and for Ali2(1,"saran");
+        this (1,"random-default-person");
+    }
+}
+
+// in java primitives are stored in the stack memory so we does not need to create any object or (primitive data types are not implemented as objects)
+
---
+++ OOP/src/introduction1/WrapperExamples.java
@@ -0,0 +1,50 @@
+package introduction1;
+
+public class WrapperExamples {
+    public static void main(String[] args) {
+        // for creating a variable of integer you can use
+        Integer a = 45; // by using this you cannot access the functions of integer using . operator
+        Integer b=35;
+        // you can also use
+        Integer num = 45;// by using this you can access the functions of integer using . operator
+        System.out.println(num.compareTo(a));
+
+        // swap func
+        swap(a,b);
+        System.out.println(a+"  "+ b); // the values will be not swapped because there is no pass by reference in java for primitives there is only pass by value
+        // by using wrapper classes we still not able to swap because Integer is a final class so we cannot change it
+
+        // final keyword
+        // by using final keyword you cannot change the value of the variable
+        final int INCREASE = 2;
+        // INCREASE = 3; // IT gives you an error because it is declared as final
+
+        // making an object as final
+
+        final New saran = new New("karthi");
+        // after making an obj you can change the values by using objnames in final
+        saran.name="saran";
+        // but if you create another object using the above obj
+        // saran = new New("new object"); you cant do it
+        System.out.println(saran.name);
+
+    }
+
+    static void swap(Integer a , Integer b){
+        Integer temp = a;
+        a=b;
+        b=temp;
+    }
+
+}
+
+class New{
+    // final int num;  always a final keyword should be initialized as the below one
+    final int num = 10;// always inititalize while declaring it...
+
+    String name;
+
+    New(String name){
+        this.name = name;
+    }
+}
